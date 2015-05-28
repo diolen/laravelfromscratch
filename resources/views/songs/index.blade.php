296f8468611f187fa4d25399fce9b0654b8ea15d
Oleg Urazbayev <oleg.urazbayev@avatarla.com>
2015-05-28 14:32:09 +0000

    Migrations and Persistence

---
+++ resources/views/songs/index.blade.php
@@ -0,0 +1,19 @@
+@extends('app')
+
+@section('content')
+<div class="container">
+	<div class="row">
+		<div class="col-md-10 col-md-offset-1">
+			<div class="panel panel-default">
+				<div class="panel-heading">Fan club</div>
+
+				<div class="panel-body">
+					@foreach($songs as $song)
+                    <li><a href="/songs/{{ $song->id }}">{{ $song->title }}</li>
+                    @endforeach
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+@endsection
